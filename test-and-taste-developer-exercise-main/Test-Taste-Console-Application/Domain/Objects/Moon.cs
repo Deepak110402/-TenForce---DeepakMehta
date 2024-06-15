4662fe69371640afeef2cd58cc9494322917c596
Deepak Mehta <58500664+Deepak110402@users.noreply.github.com>
2024-06-15 14:05:06 +0530

    Source Code Uploaded

---
+++ test-and-taste-developer-exercise-main/Test-Taste-Console-Application/Domain/Objects/Moon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+
+namespace Test_Taste_Console_Application.Domain.Objects
+{
+    public class Moon
+    {
+        public string Id { get; set; }
+        public float MassValue { get; set; }
+        public float MassExponent { get; set; }
+        public double Gravity { get; set; }
+
+        public Moon(MoonDto moonDto)
+        {
+            Id = moonDto.Id;
+            MassValue = moonDto.MassValue;
+            MassExponent = moonDto.MassExponent;
+            Gravity = CalculateGravity();
+        }
+
+        private double CalculateGravity()
+        {
+            double mass = MassValue * Math.Pow(10, MassExponent);
+            // Use a simplified formula for gravity, assuming G and radius constants.
+            return mass; // Simplified gravity estimate.
+        }
+    }
+}
---
+++ test-and-taste-developer-exercise-main/Test-Taste-Console-Application/Domain/Objects/Planet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+
+namespace Test_Taste_Console_Application.Domain.Objects
+{
+    public class Planet
+    {
+        public string Id { get; set; }
+        public float SemiMajorAxis { get; set; }
+        public ICollection<Moon> Moons { get; set; }
+        public float AverageMoonGravity
+        {
+            get
+            {
+                if (Moons == null || Moons.Count == 0)
+                {
+                    return 0;
+                }
+                return (float)Moons.Average(moon => moon.Gravity);
+            }
+        }
+
+        public Planet(PlanetDto planetDto)
+        {
+            Id = planetDto.Id;
+            SemiMajorAxis = planetDto.SemiMajorAxis;
+            Moons = new Collection<Moon>();
+            if(planetDto.Moons != null)
+            {
+                foreach (MoonDto moonDto in planetDto.Moons)
+                {
+                    Moons.Add(new Moon(moonDto));
+                }
+            }
+        }
+
+        public Boolean HasMoons()
+        {
+            return (Moons != null && Moons.Count > 0);
+        }
+    }
+}
